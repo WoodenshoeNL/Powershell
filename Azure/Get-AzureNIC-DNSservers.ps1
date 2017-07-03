c634befad2813bca6a6e5e764a744201f9a1e459
Michel Klomp <michel@woodenshoe.org>
2017-07-03 16:53:22 +0200

    Azure NIC DNS & Close file/change ACL

---
+++ Azure/Get-AzureNIC-DNSservers.ps1
@@ -0,0 +1,10 @@
+﻿
+$vms = get-azurermvm
+$nics = get-azurermnetworkinterface | where VirtualMachine -NE $null #skip Nics with no VM
+
+foreach($nic in $nics)
+{
+    $vm = $vms | where-object -Property Id -EQ $nic.VirtualMachine.id
+    $DNSservers =  $nic.DnsSettings | select-object -ExpandProperty DnsServers
+    Write-Output "$($vm.Name) : $DNSservers"
+}
---
+++ Security/close-file.ps1
@@ -0,0 +1,46 @@
+﻿
+Param(
+  [string]$file
+)
+
+$groep1 = "Domain\Beheerders"
+$groep2 = "Domain\Engineers"
+
+
+#$file = "c:\temp\test.txt"
+
+
+#file sluiten
+
+& "c:\Program Files\Unlocker\Unlocker.exe" $file /s /o
+
+
+#inheritance uitzetten file
+
+$acl = Get-ACL -Path $file
+$acl.SetAccessRuleProtection($True, $True)
+Set-Acl -Path $file -AclObject $acl
+
+$acl | fl
+#rechten groep 1
+
+$acl = Get-ACL -Path $file
+
+$permission = $groep1,"ReadAndExecute","Allow"
+$accessRule = new-object System.Security.AccessControl.FileSystemAccessRule $permission
+
+$acl.AddAccessRule($AccessRule)
+Set-Acl $file $acl
+
+#rechten groep 2
+
+$acl = Get-ACL -Path $file
+
+$permission = $groep2,"Modify","Allow"
+$accessRule = new-object System.Security.AccessControl.FileSystemAccessRule $permission
+$acl.AddAccessRule($AccessRule)
+
+
+Set-Acl $file $acl
+
+$acl | fl

eceeee2d1cf999d5ffe93b600979160247732134
Michel Klomp <michel@woodenshoe.org>
2017-12-21 17:02:00 +0100

    Folders worden nu aangemaakt met DSC

--- Azure/Single-Server/ServerPrep.ps1
+++ Azure/Single-Server/ServerPrep.ps1
@@ -1,11 +1,7 @@
-#Create Directorie
-New-Item -Path c:\install -ItemType directory
-#New-Item -Path c:\install\script -ItemType directory
 
 #Install Providers
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
-<#
 #install DSC Modules
 $requiredModules = @(
     'xPSDesiredStateConfiguration'
@@ -19,11 +15,38 @@ foreach ($module in $requiredModules) {
         Install-Module -Name $module -Force
     }
 }
-#>
+
+
+Configuration PrepConfig {
+    
+    Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+    
+    Node "localhost" {
+        
+        File InstallDir {
+            DestinationPath = "c:\install"
+            Type = "Directory" 
+            Ensure = "Present"   
+        }
+    
+        File ScriptDir {
+            DestinationPath = "c:\install\script"
+            Type = "Directory" 
+            Ensure = "Present"  
+            DependsOn = "[File]InstallDir"
+        }
+    }
+}
+    
+PrepConfig -OutputPath d:\
+    
+Start-DscConfiguration -Path d:\
+
+Sleep 30
 
 #Download Files
-#(New-Object System.Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.15.1.windows.2/Git-2.15.1.2-64-bit.exe', 'c:\install\Git-2.15.1.2-64-bit.exe')
-#(New-Object System.Net.WebClient).DownloadFile('https://download.docker.com/components/engine/windows-server/cs-1.12/docker.zip', 'c:\install\docker..zip')
+(New-Object System.Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.15.1.windows.2/Git-2.15.1.2-64-bit.exe', 'c:\install\Git-2.15.1.2-64-bit.exe')
+(New-Object System.Net.WebClient).DownloadFile('https://download.docker.com/components/engine/windows-server/cs-1.12/docker.zip', 'c:\install\docker..zip')
 
 
 #Extract Files

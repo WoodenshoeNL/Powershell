d82b915d0222b9f56360c7348bc98952a359096d
Michel Klomp <michel@woodenshoe.org>
2017-12-21 15:48:05 +0100

    Debug Prepscript deployment

--- Azure/Single-Server/ServerPrep.ps1
+++ Azure/Single-Server/ServerPrep.ps1
@@ -5,7 +5,7 @@ New-Item -Path c:\install\script -ItemType directory
 #Install Providers
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
-
+<#
 #install DSC Modules
 $requiredModules = @(
     'xPSDesiredStateConfiguration'
@@ -19,11 +19,11 @@ foreach ($module in $requiredModules) {
         Install-Module -Name $module -Force
     }
 }
-
+#>
 
 #Download Files
-(New-Object System.Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.15.1.windows.2/Git-2.15.1.2-64-bit.exe', 'c:\install\Git-2.15.1.2-64-bit.exe')
-(New-Object System.Net.WebClient).DownloadFile('https://download.docker.com/components/engine/windows-server/cs-1.12/docker.zip', 'c:\install\docker..zip')
+#(New-Object System.Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.15.1.windows.2/Git-2.15.1.2-64-bit.exe', 'c:\install\Git-2.15.1.2-64-bit.exe')
+#(New-Object System.Net.WebClient).DownloadFile('https://download.docker.com/components/engine/windows-server/cs-1.12/docker.zip', 'c:\install\docker..zip')
 
 
 #Extract Files

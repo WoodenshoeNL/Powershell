fc1aa14c8a35f8f427af64f0adc9b2abd32edca3
Michel Klomp <michel@woodenshoe.org>
2017-12-24 19:58:46 +0100

    DSC file met container role aangemaakt

--- Azure/Build-Container-Demo/Build-Container-Server/BuildContainerDSC.ps1
+++ Azure/Build-Container-Demo/Build-Container-Server/BuildContainerDSC.ps1
@@ -4,24 +4,13 @@ Configuration ContainerBuild
 
   Node $MachineName
   {
-    #Install the IIS Role
-    WindowsFeature IIS
+    #Install the Container Role
+    WindowsFeature ContainerInstall
     {
-      Ensure = �Present�
-      Name = �Web-Server�
-    }
-
-    #Install ASP.NET 4.5
-    WindowsFeature ASP
-    {
-      Ensure = �Present�
-      Name = �Web-Asp-Net45�
-    }
-
-     WindowsFeature WebServerManagementConsole
-    {
-        Name = "Web-Mgmt-Console"
         Ensure = "Present"
+        Name   = "Containers"
     }
+
+
   }
 } 

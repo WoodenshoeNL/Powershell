4ffdcb34a75e942d81cc4ae5a3af660d813fd0ed
Michel Klomp <michel@woodenshoe.org>
2017-12-29 17:24:18 +0100

    get-vmstatus

---
+++ Azure/Get-VMstatus.ps1
@@ -0,0 +1,12 @@
+﻿
+
+"Servernaam;PreMaintenanceWindowStartTime;PreMaintenanceWindowEndTime;MaintenanceWindowStartTime"
+
+foreach ($server in $(Get-AzureRmVM))
+{
+    #$server.name
+    $vmstatus = get-azurermvm -name $server.name -status -resourcegroupname $server.ResourceGroupName
+    #$($vmstatus.MaintenanceRedeployStatus).PreMaintenanceWindowStartTime
+    #$($vmstatus.MaintenanceRedeployStatus).MaintenanceWindowStartTime
+    "$($server.name);$($($vmstatus.MaintenanceRedeployStatus).PreMaintenanceWindowStartTime);$($($vmstatus.MaintenanceRedeployStatus).PreMaintenanceWindowEndTime);$($($vmstatus.MaintenanceRedeployStatus).MaintenanceWindowStartTime)"
+}

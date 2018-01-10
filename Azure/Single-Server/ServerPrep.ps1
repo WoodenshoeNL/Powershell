913828a95c93cf385e8ec04f940ff089df7aacc3
Michel Klomp <michel@woodenshoe.org>
2018-01-10 20:04:26 +0100

    Chocolatery en Boxstarter install in single server prepscript

--- Azure/Single-Server/ServerPrep.ps1
+++ Azure/Single-Server/ServerPrep.ps1
@@ -50,15 +50,16 @@ Remove-Item d:\localhost.mof -force
 
 #Download Files
 (New-Object System.Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.15.1.windows.2/Git-2.15.1.2-64-bit.exe', 'c:\install\Git-2.15.1.2-64-bit.exe')
-(New-Object System.Net.WebClient).DownloadFile('https://download.docker.com/win/static/stable/x86_64/docker-17.09.0-ce.zip', 'c:\install\docker.zip')
+#(New-Object System.Net.WebClient).DownloadFile('https://download.docker.com/win/static/stable/x86_64/docker-17.09.0-ce.zip', 'c:\install\docker.zip')
 
 
 #Extract Files
-Expand-Archive c:\install\docker.zip -DestinationPath 'C:\Program Files'
+#Expand-Archive c:\install\docker.zip -DestinationPath 'C:\Program Files'
 
 #Install Software
-
-
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+. { Invoke-WebRequest -useb http://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression 
+get-boxstarter -Force
 
 
 #Log time

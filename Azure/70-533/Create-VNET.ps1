54883c135d893642cf5cb3963e1a6c86ccede628
Michel Klomp <michel@woodenshoe.org>
2018-04-04 17:54:42 +0200

    70-533 VNET peering

---
+++ Azure/70-533/Create-VNET.ps1
@@ -0,0 +1,18 @@
+$subnets = @()
+$subnet1Name = "Apps"
+$subnet2Name = "Data"
+$subnet1AddressPrefix = "10.0.0.0/24"
+$subnet2AddressPrefix = "10.0.1.0/24"
+$vnetAddresssSpace = "10.0.0.0/16"
+$VNETName = "ExamRefVNET-PS"
+$rgName = "ExamRefRGPS"
+$location = "Central US"
+$subnets = New-AzureRmVirtualNetworkSubnetConfig -Name $subnet1Name `
+                                    -AddressPrefix $subnet1AddressPrefix
+$subnets = New-AzureRmVirtualNetworkSubnetConfig -Name $subnet2Name `
+                                    -AddressPrefix $subnet2AddressPrefix
+$vnet = New-AzureRmVirtualNetwork -Name $VNETName `
+                          -ResourceGroupName $rgName `
+                          -Location $location `
+                          -AddressPrefix $vnetAddresssSpace `
+                          -Subnet $subnets

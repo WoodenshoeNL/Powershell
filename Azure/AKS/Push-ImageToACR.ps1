da4450c90dc9174a820fc85bf74a8da78745d011
Michel Klomp <michel@woodenshoe.org>
2018-04-10 15:32:20 +0200

    ander docker image in het push script

--- Azure/AKS/Push-ImageToACR.ps1
+++ Azure/AKS/Push-ImageToACR.ps1
@@ -11,11 +11,11 @@ docker login $registry.LoginServer -u $creds.Username -p $creds.Password
 
 
 #pull Image
-docker pull microsoft/aci-helloworld
+docker pull microsoft/iis
 
 #Tag Image
-$image = $registry.LoginServer + "/aci-helloworld:v1"
-docker tag microsoft/aci-helloworld $image
+$image = $registry.LoginServer + "/iis:v1"
+docker tag microsoft/iis $image
 
 #Push Image to ACR
 docker push $image

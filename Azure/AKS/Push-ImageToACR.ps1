92246f335dd6991210049d373e4f0791e2593589
Michel Klomp <michel@woodenshoe.org>
2018-04-10 15:09:56 +0200

    Push Docker Image to ACR

---
+++ Azure/AKS/Push-ImageToACR.ps1
@@ -0,0 +1,20 @@
+
+
+$resourceGroup = "ContainerTest"
+$RegistryName = "Woodenshoe"
+
+
+#Login Registry
+$creds = Get-AzureRmContainerRegistryCredential -RegistryName $RegistryName -ResourceGroupName $resourceGroup
+docker login $registry.LoginServer -u $creds.Username -p $creds.Password
+
+
+#pull Image
+docker pull microsoft/aci-helloworld
+
+#Tag Image
+$image = $registry.LoginServer + "/aci-helloworld:v1"
+docker tag microsoft/aci-helloworld $image
+
+#Push Image to ACR
+docker push $image

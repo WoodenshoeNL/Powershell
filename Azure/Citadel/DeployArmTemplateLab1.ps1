b6821c08602c4327915fd4b4aa330b21f3f77d71
Michel Klomp <michel@woodenshoe.org>
2018-05-10 23:13:08 +0200

    Azure Citadel Arm Template parameter file

--- Azure/Citadel/DeployArmTemplateLab1.ps1
+++ Azure/Citadel/DeployArmTemplateLab1.ps1
@@ -1,5 +1,6 @@
 $rg="lab1"
 $template="C:\GIT\Powershell\Azure\Citadel\azuredeploy.json"
+$parms="C:\GIT\Powershell\Azure\Citadel\azuredeploy.parameter.json"
 $job="job2"
 $storageAccount="citadelsa"
-New-AzureRmResourceGroupDeployment -Name $job -storageAccountPrefix $storageAccount -TemplateFile $template -ResourceGroupName $rg
+New-AzureRmResourceGroupDeployment -Name $job -storageAccountPrefix $storageAccount -TemplateParameterFile $parms -TemplateFile $template -ResourceGroupName $rg

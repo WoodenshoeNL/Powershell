aceacfda140e4d5816e795d3250e1e8a9f4546d8
Michel Klomp <michel@woodenshoe.org>
2018-05-10 15:52:14 +0200

    Azure Citadel Arm Template deploy script

---
+++ Azure/Citadel/DeployArmTemplateLab1.ps1
@@ -0,0 +1,5 @@
+rg="lab1"
+$template="C:\GIT\Powershell\Azure\Citadel\azuredeploy.json"
+$job="job2"
+$storageAccount="citadeltestsa2"
+New-AzureRmResourceGroupDeployment -Name $job -storageAccount $storageAccount -TemplateFile $template -ResourceGroupName $rg

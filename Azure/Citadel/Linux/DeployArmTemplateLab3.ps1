b6e48ac01d02d6866d91e6c939bf224711bab5a5
Michel Klomp <michel@woodenshoe.org>
2018-05-21 22:30:54 +0200

    Azure Citadel refactoring lab3

--- Azure/Citadel/Linux/DeployArmTemplateLab3.ps1
+++ Azure/Citadel/Linux/DeployArmTemplateLab3.ps1
@@ -1,6 +1,5 @@
-$rg="lab1"
-$template="C:\GIT\Powershell\Azure\Citadel\linux\azuredeploy.json"
-$parms="C:\GIT\Powershell\Azure\Citadel\linux\azuredeploy.parameter.json"
-$job="job3"
-$storageAccount="citadelsa"
-New-AzureRmResourceGroupDeployment -Name $job -storageAccountPrefix $storageAccount -TemplateParameterFile $parms -TemplateFile $template -ResourceGroupName $rg
+$resourceGroupName="lab1"
+$templateFilePath="C:\GIT\Powershell\Azure\Citadel\linux\azuredeploy.json"
+$parametersFilePath="C:\GIT\Powershell\Azure\Citadel\Linux\azuredeploy.parameters.json"
+
+New-AzureRmResourceGroupDeployment -TemplateParameterFile $parametersFilePath -TemplateFile $templateFilePath -ResourceGroupName $resourceGroupName -Verbose

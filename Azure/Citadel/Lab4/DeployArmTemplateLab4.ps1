a30eca39daf41c32b320f7aba85d307467e8efc9
Michel Klomp <michel@woodenshoe.org>
2018-05-22 15:30:25 +0200

    Azure Citadel Conditional PIP

--- Azure/Citadel/Lab4/DeployArmTemplateLab4.ps1
+++ Azure/Citadel/Lab4/DeployArmTemplateLab4.ps1
@@ -1,5 +1,5 @@
 $resourceGroupName="lab1"
-$templateFilePath="C:\GIT\Powershell\Azure\Citadel\linux\azuredeploy.json"
-$parametersFilePath="C:\GIT\Powershell\Azure\Citadel\Linux\azuredeploy.parameters.json"
+$templateFilePath="C:\GIT\Powershell\Azure\Citadel\Lab4\azuredeploy.json"
+$parametersFilePath="C:\GIT\Powershell\Azure\Citadel\Lab4\azuredeploy.parameters.json"
 
 New-AzureRmResourceGroupDeployment -TemplateParameterFile $parametersFilePath -TemplateFile $templateFilePath -ResourceGroupName $resourceGroupName -Verbose

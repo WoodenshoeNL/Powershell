0af511ae0b1b899c147580a14008a91e527896b7
Michel Klomp <michel@woodenshoe.org>
2018-05-23 13:47:06 +0200

    SCOM Enable en Disable Notification Subscriptions

---
+++ SCOM/MM-24subscriptions/Enable-MM-24h-Subscriptions.ps1
@@ -0,0 +1,10 @@
+﻿
+#Get-SCOMNotificationSubscription | ?{$_.DisplayName -match "24h"}  | Select-object DisplayName
+
+$subscriptions = get-content -Path .\subscriptions.txt
+
+foreach($subscription in $subscriptions)
+{
+    $subscription
+    Get-SCOMNotificationSubscription $subscription | Enable-SCOMNotificationSubscription
+}

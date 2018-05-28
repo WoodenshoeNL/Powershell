aabe22369c0a3ae50781b4720241f050101964b6
Michel Klomp <michel@woodenshoe.org>
2018-05-28 17:23:10 +0200

    Server cleanup script

---
+++ Diverse/Server-Cleanup/CreateScriptDir.cmd
@@ -0,0 +1,8 @@
+md C:\script
+
+xcopy "\\mgmt-rds02.management.int\c$\Operations Manager\Agent_2016\Scripts\Cleanup" "C:\script"
+
+timeout 5
+
+explorer c:\script
+
---
+++ Diverse/Server-Cleanup/server-cleanup.ps1
@@ -0,0 +1,74 @@
+##Init Arrays
+
+$iisLogs = @(
+    "C:\inetpub\logs\LogFiles"
+)
+
+$cleanupDirs = @(
+    "C:\Program Files\Microsoft Data Protection Manager\DPM\Temp",
+    "C:\ProgramData\Microsoft\Windows\WER\ReportQueue",
+    "C:\ProgramData\Microsoft\Windows\WER\ReportArchive",
+    "C:\Windows\SoftwareDistribution\Download",
+    "C:\Windows\ccmcache",
+    "C:\Windows\Temp",
+    "C:\WER\ReportQueue",
+    "C:\WER\ReportArchive",
+    "C:\temp"
+)
+
+$userDownloads = @(
+    "C:\Users\*\Downloads"
+)
+
+##Start Clean up
+
+foreach ($dir in $cleanupDirs) {
+    if (Test-Path $dir ) {
+        Write-Host "[*] Folder found: $dir" -ForegroundColor "green"
+        $teller = 0
+        foreach ($item in Get-ChildItem -Path $dir) {
+            if ($item.LastWriteTime -lt (Get-Date).AddDays(-14)) {
+                $teller++
+                Remove-Item $item.FullName -Force -Recurse
+            }
+        }
+        Write-Host "[-] $teller Items removed" -ForegroundColor "DarkYellow"
+    }
+    
+}
+
+foreach ($dir in $iisLogs) {
+    if (Test-Path $dir ) {
+        foreach ($folder in Get-ChildItem -Path $dir) {
+            Write-Host "[*] Folder found: $($folder.fullname)" -ForegroundColor "green"
+            $teller = 0
+            foreach ($item in Get-ChildItem -Path $folder.fullname) {
+                if ($item.LastWriteTime -lt (Get-Date).AddDays(-14)) {
+                    $teller++
+                    #$item.FullName
+                    Remove-Item $item.FullName -Force
+                }
+            }
+            Write-Host "[-] $teller Items removed" -ForegroundColor "DarkYellow"
+        }
+    }
+}
+
+foreach ($dir in $userDownloads) {
+    if (Test-Path $dir ) {
+        foreach ($folder in Get-ChildItem -Path $dir) {
+            if (Test-Path  $folder.fullname ) {
+                Write-Host "[*] Folder found: $($folder.fullname)" -ForegroundColor "green"
+                $teller = 0
+                foreach ($item in Get-ChildItem -Path $folder.fullname) {
+                    if ($item.LastWriteTime -lt (Get-Date).AddDays(-30)) {
+                        $teller++
+                        $item.FullName
+                        #Remove-Item $item.FullName -Force
+                    }
+                }
+                Write-Host "[-] $teller Items found" -ForegroundColor "DarkYellow"
+            }
+        }
+    }
+}

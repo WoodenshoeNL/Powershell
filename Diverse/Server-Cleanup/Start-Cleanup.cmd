4c69958654c3284d98d21ba800f7976a81848fc3
Michel Klomp <michel@woodenshoe.org>
2018-05-30 16:49:33 +0200

    start-cleanup script - set-location

--- Diverse/Server-Cleanup/Start-Cleanup.cmd
+++ Diverse/Server-Cleanup/Start-Cleanup.cmd
@@ -2,7 +2,7 @@
 
 robocopy "\\tsclient\C\Operations Manager\_Script\Cleanup" "C:\script\Cleanup"
 
-powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-noexit','-command &{C:\script\cleanup\server-cleanup.ps1}'"
+powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-noexit','-command &{C:\script\cleanup\server-cleanup.ps1;Set-Location C:\script\Cleanup}'"
 
 timeout 5
 

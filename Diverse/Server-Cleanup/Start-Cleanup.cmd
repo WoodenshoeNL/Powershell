b277a6479e6569921cb7dcbe6ee4eff02271e660
Michel Klomp <michel@woodenshoe.org>
2018-05-30 15:24:15 +0200

    One click copy & start cleanup script

---
+++ Diverse/Server-Cleanup/Start-Cleanup.cmd
@@ -0,0 +1,10 @@
+#md C:\script
+
+robocopy "\\tsclient\C\Operations Manager\_Script\Cleanup" "C:\script\Cleanup"
+
+powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-noexit','-command &{C:\script\cleanup\server-cleanup.ps1}'"
+
+timeout 5
+
+
+

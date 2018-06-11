10e6a42cb576a833ac8ba9e95ef468eaf3e7a8d3
Michel Klomp <michel@woodenshoe.org>
2018-06-11 16:09:29 +0200

    Report HealthService DB size

--- SCOM/Reset-HealthServiceDB/Reset-HealthServiceDB.ps1
+++ SCOM/Reset-HealthServiceDB/Reset-HealthServiceDB.ps1
@@ -4,3 +4,7 @@ $healthServiceDB = "$env:programfiles\Microsoft Monitoring Agent\Agent\Health Se
 Stop-Service healthservice
 Remove-Item $healthServiceDB -Force
 Start-Service healthservice
+
+Start-Sleep 10
+
+"HealthService DB Size: " + $(((Get-Item "$env:programfiles\Microsoft Monitoring Agent\Agent\Health Service State\Health Service Store\HealthServiceStore.edb").length) / 1mb)

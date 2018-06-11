6154d9ca9bd97328ab3f7f16eea9b42bd6c87af0
Michel Klomp <michel@woodenshoe.org>
2018-06-11 13:57:02 +0200

    Execution Policy ByPass Start Reset script

--- SCOM/Reset-HealthServiceDB/Start-Reset.cmd
+++ SCOM/Reset-HealthServiceDB/Start-Reset.cmd
@@ -2,7 +2,7 @@
 
 robocopy "\\tsclient\C\Operations Manager\_Script\Reset-HealthServiceDB" "C:\script\Reset-HealthServiceDB"
 
-powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-noexit','-command &{C:\script\Reset-HealthServiceDB\Reset-HealthServiceDB.ps1;Set-Location C:\script\Reset-HealthServiceDB}'"
+powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-noexit -ExecutionPolicy Bypass','-command &{C:\script\Reset-HealthServiceDB\Reset-HealthServiceDB.ps1;Set-Location C:\script\Reset-HealthServiceDB}'"
 
 timeout 30
 

dcd0ea48451d976b9cae8899aed4f7056d214aa9
Michel Klomp <michel@woodenshoe.org>
2018-06-25 14:55:04 +0200

    Start cleanup bypass execution policy

--- Diverse/Server-Cleanup/Start-Cleanup.cmd
+++ Diverse/Server-Cleanup/Start-Cleanup.cmd
@@ -2,7 +2,7 @@
 
 robocopy "\\tsclient\C\Operations Manager\_Script\Cleanup" "C:\script\Cleanup"
 
-powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-noexit','-command &{C:\script\cleanup\server-cleanup.ps1;Set-Location C:\script\Cleanup}'"
+powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-noexit -ExecutionPolicy Bypass','-command &{C:\script\cleanup\server-cleanup.ps1;Set-Location C:\script\Cleanup}'"
 
 timeout 5
 
